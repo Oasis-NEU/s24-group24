86472c3588136b1aafe243b78245a8988f24d916
Smyan Sengupta <senguptasmyan@gmail.com>
2024-03-18 02:46:20 -0400

    Added method to calculate grade in course

--- src/course.ts
+++ src/course.ts
@@ -1,12 +1,14 @@
 class Course {
     name: string;
     prof: string;
+    credits: number;
     assignments: Assignment[];
     weights: Map<string, number>;
 
-    constructor(name: string, prof: string, assignments?: Assignment[]) {
+    constructor(name: string, prof: string, credits: number, assignments?: Assignment[]) {
         this.name = name;
         this.prof = prof;
+        this.credits = credits;
         if (assignments === undefined) {
             this.assignments = [];
         } else {
@@ -21,5 +23,21 @@ class Course {
         } else {
             this.assignments.push(new Assignment(name, category, totalPoints, score));
         }
+    }
+
+    numberInCategory(cat: string): number {
+        return this.assignments.filter(assignment => assignment.category === cat).length;
+    }
+
+    calculateGrade(): number {
+        let grade: number = 0;
+        this.assignments.forEach((assignment) => {
+            if (this.weights.has(assignment.category) && assignment.isGraded) {
+                grade += assignment.score()
+                * ((this.weights.get(assignment.category) as number)
+                / this.numberInCategory(assignment.category));
+            }
+        });
+        return grade;
     }
 }
